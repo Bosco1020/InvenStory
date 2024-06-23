1621777768bb4c0e52e4054ffc23284dab6eac1e
[Connor Boscherini] <connorbos@hotmail.co.uk>
2024-06-23 18:03:40 +0100

    FEAT: Developed Authentication to validate and make HTTP requests

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import Login from "./components/Login.jsx";
 import SignUp from "./components/SignUp.jsx";
 
+import Test from "./components/Test.jsx"; // Development only
+
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
@@ -16,6 +18,12 @@ const App = () => {
   return (
     <div className="container-fluid">
       <Routes>
+        <Route
+          path="/"
+          element={
+            !loggedIn ? <Login setLoggedIn={changeLoggedIn} /> : <Test />
+          }
+        />
         <Route
           path="/SignUp"
           element={<SignUp setLoggedIn={changeLoggedIn} />}
--- src/components/AccountForm.jsx
+++ src/components/AccountForm.jsx
@@ -1,30 +1,62 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-import { useState } from "react";
+import { login, signUp } from "../service/auth.service.js";
+import {
+  validateEmail,
+  validatePassword,
+} from "../middleware/form.validation.js";
+
+import "./CSS/Form.css";
 
 const AccountForm = ({ changeLoggedIn, newAccount }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [goodEmail, setGoodEmail] = useState("Valid");
+  const [goodPassword, setGoodPassword] = useState("Valid");
+
   let navigate = useNavigate();
+
+  const updateEmailValidation = (updateEmail) => {
+    if (!newAccount) return; //Only show help text if creating
+    if (validateEmail(updateEmail)) setGoodEmail("Valid");
+    else setGoodEmail("Invalid");
+  };
+
+  const updatePasswordValidation = (updatePassword) => {
+    if (!newAccount) return;
+    if (validatePassword(updatePassword)) setGoodPassword("Valid");
+    else setGoodPassword("Invalid");
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (localStorage.getItem("user")) {
-      // check for "user" token
-      changeLoggedIn(true);
-      navigate(`/`); // send after login
+    if (!goodEmail || !goodPassword) return;
+
+    let response;
+    if (!newAccount) {
+      response = await login(email, password);
+      if (localStorage.getItem("user")) changeLoggedIn(true); //? May not need
     } else {
-      changeLoggedIn(false);
+      response = await signUp(email, password);
+      if (response._id) navigate(`/Login`);
     }
+
+    console.log(response);
   };
 
   useEffect(() => {
     if (localStorage.getItem("user")) {
       changeLoggedIn(true);
+      navigate(`/`); // redirect if already logged in
+    }
+    /* //? Check if need, should be ok without
+    if (localStorage.getItem("user")) {
+      changeLoggedIn(true);
     } else changeLoggedIn(false);
+    */
   }),
     [];
 
@@ -37,15 +69,26 @@ const AccountForm = ({ changeLoggedIn, newAccount }) => {
             <input
               type="text"
               name="emailInput"
-              className="InputEmail"
+              className={`Input${goodEmail}`}
               id="emailInput"
               value={email}
               placeholder="example@email.com"
               onChange={(e) => {
                 setEmail(e.target.value);
+                updateEmailValidation(e.target.value);
               }}
             />
           </label>
+          {goodEmail == "Invalid" ? (
+            <>
+              <br />
+              <label>
+                <p className="Invalid">Please enter a valid email address</p>
+              </label>
+            </>
+          ) : (
+            <></>
+          )}
           <br />
           <br />
           <label>
@@ -53,25 +96,35 @@ const AccountForm = ({ changeLoggedIn, newAccount }) => {
             <input
               type="password"
               name="passwordInput"
-              className="InputPassword"
+              className={`Input${goodPassword}`}
               id="passwordInput"
               value={password}
               placeholder="Enter Password"
               onChange={(e) => {
                 setPassword(e.target.value);
+                updatePasswordValidation(e.target.value);
               }}
             />
           </label>
+          {goodPassword == "Invalid" ? (
+            <>
+              <br />
+              <label>
+                <>Password must be at least 8 characters long,</>
+                <br />
+                <>contain 1 uppercase letter,</>
+                <br />
+                <>a number and a special character</>
+                <br />
+              </label>
+            </>
+          ) : (
+            <></>
+          )}
           <br />
           <br />
           <div className="d-flex justify-content-center">
-            <button
-              type="submit"
-              className="btn btn-primary"
-              disabled={() => {
-                return newAccount;
-              }}
-            >
+            <button type="submit" className="btn btn-primary">
               Submit
             </button>
           </div>
@@ -81,5 +134,9 @@ const AccountForm = ({ changeLoggedIn, newAccount }) => {
     </div>
   );
 };
+
+// disabled={() => {
+//                 return newAccount || (goodEmail && goodPassword);
+//               }}
 
 export default AccountForm;
