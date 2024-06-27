79e580d5f8059d2d0adcca91a8277c4731e14529
[Connor Boscherini] <connorbos@hotmail.co.uk>
2024-06-27 10:54:54 +0100

    REACT: Developed view page to hide/reveal filter form

--- src/components/FilterItems.jsx
+++ src/components/FilterItems.jsx
@@ -1,7 +1,103 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
-const FilterItems = () => {
-  return <div></div>;
+const FilterItems = ({ Filters }) => {
+  const [filterOpen, setFilterOpen] = useState(false);
+  const [nameFilter, setNameFilter] = useState("");
+  const [tagFilter, setTagFilter] = useState("");
+
+  const handleFilter = () => {
+    Filters({ name: nameFilter, tag: tagFilter });
+    //window.location.href = "/"; // Reload view page after setting filters
+  };
+
+  const closeFilter = async (e) => {
+    // e.handleDefault();
+    setFilterOpen(false);
+  };
+
+  const openFilter = async (e) => {
+    //e.handleDefault();
+    setFilterOpen(true);
+  };
+
+  const clearFilter = () => {
+    setNameFilter("");
+    setTagFilter("");
+    Filters({ name: "", tag: "" });
+  };
+
+  return (
+    <div>
+      {filterOpen ? (
+        <div className="container">
+          <form onSubmit={handleFilter}>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="nameFilter"
+                className="nameFilter"
+                id="nameFilter"
+                value={nameFilter}
+                placeholder="Search by Name"
+                onChange={(e) => {
+                  setNameFilter(e.target.value);
+                }}
+              />
+            </label>
+            <label>
+              Tag:
+              <input
+                type="text"
+                name="tagFilter"
+                className="tagFilter"
+                id="tagFilter"
+                value={tagFilter}
+                placeholder="Search by Tag"
+                onChange={(e) => {
+                  setTagFilter(e.target.value);
+                }}
+              />
+            </label>
+            <button type="submit" className="btn btn-primary">
+              Search
+            </button>
+          </form>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            variant="secondary"
+            size="sm"
+            onClick={closeFilter}
+          >
+            Close Filter
+          </button>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            variant="secondary"
+            size="sm"
+            onClick={clearFilter}
+          >
+            Clear
+          </button>
+        </div>
+      ) : (
+        <div className="container">
+          <button
+            type="button"
+            className="btn btn-primary"
+            variant="primary"
+            size="lg"
+            onClick={openFilter}
+          >
+            Filter
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default FilterItems;
--- src/components/ViewItems.jsx
+++ src/components/ViewItems.jsx
@@ -1,20 +1,51 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
+import { getItemsData } from "../service/items.service.js";
 import FilterItems from "./FilterItems.jsx";
 import ItemsTable from "./ItemsTable.jsx";
-import ItemBox from "./ItemBox.jsx";
 
-import dummyData from "../../data/dummyItems.json";
-
-/* <FilterItems /> */
+//import dummyData from "../../data/dummyItems.json";
 
 const ViewItems = () => {
-  console.log(dummyData);
+  const [loading, setLoading] = useState(true);
+  const [filters, setFilters] = useState({ name: "", tag: "" });
+  const [allItems, setAllItems] = useState([]);
+
+  const filterItems = () => {
+    // Apply filter to items shown
+  };
+
+  useEffect(() => {
+    const getItems = async () => {
+      const res = await getItemsData();
+      console.log(res);
+      setAllItems(res);
+      setLoading(false);
+    };
+
+    if (!loading) return;
+    getItems();
+    // Check if filters applied
+    if (filters.name == "" || filters.tag == "") return;
+    filterItems();
+  }),
+    [];
+
   return (
     <div>
       <br />
       <br />
-      <ItemsTable allItems={dummyData.allItems} />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <br />
+          <FilterItems Filters={setFilters} />
+          <br />
+          <ItemsTable allItems={allItems} />
+        </>
+      )}
     </div>
   );
 };
