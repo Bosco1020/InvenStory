022a6391866afb36e0f7ac971a6fdc3b467085a0
[Connor Boscherini] <connorbos@hotmail.co.uk>
2024-07-03 21:26:09 +0100

    REACT-itemFIlterServicetTests: Tests created and passed for item filter service

--- tests/data/testItems.js
+++ tests/data/testItems.js
@@ -41,7 +41,7 @@ const getTestItems = async () => {
                     "_id": "66828e1a5d51321703713e06",
                     "name": "Buzz Movie",
                     "description": "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little.",
-                    "tagList": []
+                    "tagList": ["Forest"]
             }
         ],
 
@@ -80,7 +80,7 @@ const getTestItems = async () => {
             {
                     "name": "Buzz Movie",
                     "description": "Sample Test",
-                    "tagList": []
+                    "tagList": ["Forest"]
             }
         ]
     }
